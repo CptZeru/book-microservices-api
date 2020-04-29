c710ba9d6647b572ed9685d38654aa09bcdd7f11
CptZeru <zerulegend@gmail.com>
2020-04-29 15:06:20 +0700

    add console log to route.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,15 @@
 const bookRoutes = require('./books')
 
 const appRouter = (app, fs) => {
+    console.log("entering approuter");
     //default route
     app.get('/', (req,res)=>{
+        console.log('entering get / route');
         res.send('welcome to microservices book api');
     });
 
     bookRoutes(app,fs);
+    console.log("after bookRoutes");
 };
 
 module.exports = appRouter;
