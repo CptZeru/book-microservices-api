4c1678b463313ff2e590b03638cfee34000768e6
CptZeru <zerulegend@gmail.com>
2020-04-29 12:20:56 +0700

    adding cors package, for heroku purpose

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 // source : https://robkendal.co.uk/blog/how-to-build-a-restful-node-js-api-server-using-json-files/
+// source api deploy to heroku : https://dev.to/lucianopereira86/uploading-a-nodejs-web-api-to-heroku-32kn
 
 // load express & body-parser package
 const express = require('express');
 const bodyParser = require('body-parser');
+var cors = require('cors');
 
 // create instance of express for serving end points
 const app = express();
@@ -10,6 +12,13 @@ const app = express();
 //load filesystem
 const fs = require('fs');
 
+app.use(
+    cors({
+        credentials: true,
+        origin: true
+    })
+);
+app.options('*', cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
